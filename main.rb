1612e0dc127603e9dc52552a72ffcd9948c976e3
Jonathan Smith <jonathan@impacti.org>
2011-09-11 19:43:28 +0100

    hoorah! it works

--- main.rb
+++ main.rb
@@ -2,12 +2,12 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'haml'
 require 'datamapper'
+require 'ruby-debug'
 
 require 'awesome_print' if development?
 require 'date'
 
 DataMapper::setup(:default, "sqlite3:challenge.db")
-
 
 class Challenge
   # To start, will only have one challenge - "go running"
@@ -18,8 +18,11 @@ class Challenge
   property :aim, String
   property :description, String
   # property :first, DateTime
+  property :duration, Integer
   property :longest_chain, Integer
-  property :current_chain, Integer
+  property :current_chain, Integer 
+  property :done, Integer
+  property :missed, Integer
 
   
   def setup_sample
@@ -46,27 +49,28 @@ class Challenge
   end
 
   def check_time_since_longest
-    if @longest_chain > @current_chain
-      @time_since_longest += 1
-    else @time_since_longest = 0
+    if self.longest_chain > self.current_chain
+      self.time_since_longest += 1
+    else self.time_since_longest = 0
     end
   end
 
   def update(action) # 0 = missed, 1 = done
     if action == 1
-      @current_chain += 1
-      @done += 1
-      @time_since_tick = 0
-      @chain_record << 1
+      self.current_chain += 1
+      self.done += 1
+      # @time_since_tick = 0
+      # @chain_record << 1
     else
-      @current_chain = 0
-      @time_since_tick += 1
-      @missed += 1
-      @chain_record << 0
+      self.current_chain = 0
+      # @time_since_tick += 1
+      self.missed += 1
+      # @chain_record << 0
     end
-    @duration += 1
+    self.duration += 1
     check_if_longest
-    check_time_since_longest
+    self.save
+    # check_time_since_longest
   end
 
   def shorten_record
@@ -91,16 +95,43 @@ class Web
   get '/setup' do
     # @c = Challenge.new
     # @c.setup_sample
-    Challenge.create(:aim => "new one")
+    Challenge.create(
+      :aim => "new one", 
+      :description => "Go running for 15 mins", 
+      :duration => 9,
+      :longest_chain => 5,
+      :current_chain => 2,
+      :done => 7,
+      :missed => 2
+      )
     "Made new resource"
   end
   
   get '/display' do
     # @c = Challenge.new#.setup_sample
     # @c.setup_sample
-    @c = Challenge.get
+    @c = Challenge.first
+    puts "@c is #{@c}" 
     haml :display
   end
+  
+  
+  get '/done' do
+    @c = Challenge.first
+    # debugger
+    @c.update(1)
+    haml :display
+  end
+
+  get '/missed' do
+    @c = Challenge.first
+    @c.update(0)
+    haml :display
+  end
+
+  # get '/create' do
+  #   haml :
+  # end
   
     
 end
